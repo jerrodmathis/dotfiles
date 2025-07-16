b31efc0cd5fee49b87621fcae874881801013e7b
Jerrod Mathis <jerrodmathis@proton.me>
2025-07-15 21:14:02 -0400

    adding fresh.sh

---
+++ fresh.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "Performing a fresh install..."
+
+# Check for oh-my-zsh and install if we don't have it
+if test ! $(which omz); then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all dependencies with bundle (see Brewfile)
+brew tap homebrew/bundle
+brew bundle --file ./Brewfile
+
+# Create directories
+mkdir $HOME/code
+
+# Create code subdirectories
+mkdir $HOME/code/intellistack
+mkdir $HOME/code/jerrodmathis
+
+# Clone repositories
+./clone.sh
+
+# Create symlinks with Stow
+stow .
