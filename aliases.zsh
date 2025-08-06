5628477dc4f8e3a5e92b665d267f25d5a2f152a2
Jerrod Mathis <jerrodmathis@proton.me>
2025-08-06 17:22:18 -0400

    add aliases file

---
+++ aliases.zsh
@@ -0,0 +1,5 @@
+alias ui="cd $HOME/code/intellistack/odin-ui/packages/web-app/"
+alias da="cd $HOME/code/intellistack/daedalus/"
+alias wf="cd $HOME/code/intellistack/odin-workflows/"
+
+alias vim="nvim"
